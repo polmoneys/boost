8fb31320cadf15e5ba5bbdc2d4249f490e089e59
Pol Moneys <pmones@wideagency.com>
2023-01-26 17:05:51 +0100

    chores : improve date mgmt on packages/Utils

--- packages/Utils/src/date.ts
+++ packages/Utils/src/date.ts
@@ -1,3 +1,12 @@
+import ms from "ms";
+
+export const timeAgo = (timestamp: Date, timeOnly?: boolean): string => {
+  if (!timestamp) return "never";
+  return `${ms(Date.now() - new Date(timestamp).getTime())}${
+    timeOnly ? "" : " ago"
+  }`;
+};
+
 /**
  * Utility to format a Date according to a locale
  */
@@ -12,3 +21,53 @@ export function formatReadingTime(minutes: number) {
   let cups = Math.round(minutes / 5);
   return `${new Array(cups || 1).fill("☕️").join("")} ${minutes} min read`;
 }
+
+/**
+ * Utility for month mgmt
+ * console.log(month.name(2));
+ * → March
+ * console.log(month.number("November"));
+ * → 10
+ */
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+] as const;
+
+type MonthName = typeof months[number];
+
+export const monthMgmt = (function () {
+  const names: Array<MonthName> = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  return {
+    name: function (number: number) {
+      return names[number];
+    },
+    number: function (name: MonthName) {
+      return names.indexOf(name);
+    },
+  };
+})();
